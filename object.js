b3f2fdc88b7018b69e6685c9cf1fa54e2883b6ed
nirmal jangid <jangidnirmal15@gmail.com>
2025-06-05 21:30:17 +0530

    object program

---
+++ object.js
@@ -0,0 +1,35 @@
+// const insta = {
+//     username : "_nirmal15",
+//     follwers : 560,
+//     like : 2234,
+//     tag : ["@aman ","@hanumaji","@jai baba ki"]
+
+// };
+// console.log(insta);
+
+// nesting object
+// const studeninfo ={
+//      nirmal :{
+//            gread : "B",
+//            place : "muhi"
+//     },
+//     aman :{
+//         gread : 'A',
+//         place : "khonde"
+//     },
+//     nisha : {
+//         target : "for me",
+//         local : "don"
+//     },
+// };
+// console.log(studeninfo[1]);
+
+//random number 1 to 100
+
+// const random = Math.floor((Math.random()*99)+1);
+// console.log(random)
+
+// 1 to 5
+
+const num = Math.floor(Math.random()*5)+1;
+console.log(num)
