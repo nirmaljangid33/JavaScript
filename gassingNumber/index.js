0c49273025018f60358c2299532e332b880d57ae
nirmal jangid <jangidnirmal15@gmail.com>
2025-06-05 22:43:02 +0530

    guessing number add tha large and small

--- gassingNumber/index.js
+++ gassingNumber/index.js
@@ -1,16 +1,22 @@
 let data = prompt("enter the what do you");
-while(true){
-    if(data =="play"){
 let num = prompt("enter the number of range");
 let random = Math.floor((Math.random()*num)+1);
-        let gusing = prompt("gussing the number ");
+let gusing = prompt("gussing the number ");
+while(true){
+    if(data =="play"){
+
         if(random == gusing){
             console.log(random);
             console.log("oo my good you are gassing the right number ");
                    break;
         }
-        else{
-          gusing = prompt("enter enter the number try agin");
+            else if(random <gusing){
+                console.log("random number is small",random);
+                gusing = prompt("enter enter the number try agin");
+        }
+        else {
+                console.log("random number is large",random);
+                gusing = prompt("enter enter the number try agin");
         }
     }
     else if(data =="quite"){
