33cdbc5786c7120404dc8e2661ce96741a328fd7
nirmal jangid <jangidnirmal15@gmail.com>
2025-06-08 21:08:07 +0530

    function Expression

---
+++ function.js
@@ -0,0 +1,26 @@
+// function dice(){
+//       let num = Math.floor((Math.random()*6)+1);
+//       console.log("dice is ", num);
+// }
+// dice();
+// concation
+// let num = ["nirml" ,"jangid","kumar","jai hanuman ","jai baba li"];
+// function concate(){
+//     let result = num.trim().num.join();
+     
+    
+//         console.log(result);
+// }
+// concate();
+
+// function Expressions
+
+let hello = function(){
+    console.log("jai shree ram");
+}
+hello();
+
+hello = function(){
+    console.log("jai baba ki");
+}
+hello();
