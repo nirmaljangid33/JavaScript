9e1a19d7bc6c470b9d2020f8293013f3e2820403
nirmal jangid <jangidnirmal15@gmail.com>
2025-06-08 22:29:36 +0530

    method

--- function.js
+++ function.js
@@ -15,12 +15,49 @@
 
 // function Expressions
 
-let hello = function(){
-    console.log("jai shree ram");
-}
-hello();
+// let hello = function(){
+//     console.log("jai shree ram");
+// }
+// hello();
 
-hello = function(){
-    console.log("jai baba ki");
+// hello = function(){
+//     console.log("jai baba ki");
+// }
+// hello();
+
+
+// function multigrrd(fun, count){ //higher order function
+//     for(let i=0; i<count; i++){
+//        fun();
+//     }
+// }
+//     function greed(){
+//         console.log("nirmal jangid");
+//     }
+// multigrrd(greed, 2);
+
+// function parant(fun ,count){
+//     for(let i=1;i<=count;i++){
+//         fun();
+//     }
+// }
+
+
+// function number(){
+//     console.log("9587228230");
+// }
+// parant(number, 5);
+
+// methods
+let data ={
+    num : function(){
+        console.log(95451651656);;
+    },
+    name : function(){
+        console.log("nirmal");
+    },
+    roll : function(){
+        console.log(697010);
+    }
 }
-hello();
+data.name();
