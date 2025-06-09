c1759febb9fd346cc50c7c87a5b75c5e843206fe
nirmal jangid <jangidnirmal15@gmail.com>
2025-06-09 07:33:06 +0530

    try catch

---
+++ tryCatch.js
@@ -0,0 +1,17 @@
+console.log("nirmal jangid");
+
+try{
+    console.log(a); // a is not defined 
+} catch(e){
+    console.log(`Error is ...${e}`);//Error is ...ReferenceError: a is not defined
+}
+console.log("jai hanuman ji");
+
+console.log("jai baba ki");
+let a =10;
+try{
+    console.log(a);  // do'not erros 
+} catch(e){
+    console.log(e);
+}
+console.log("gunnu jangid");
